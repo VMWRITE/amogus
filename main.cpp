d0ace1fd94a58e56de8a04f17ced496f5a0b7c3c
Kernel <64151344+bsendpacket@users.noreply.github.com>
2021-05-31 21:54:44 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define ඞඞඞඞඞඞ u8
+#define ඞ int
+#define ඞඞ main
+#define ඞ_ඞ <<
+#define ඞඞඞ printf
+#define ඞඞඞඞ "ඞ"
+#define ඞඞඞඞඞ getchar
+
+ඞ ඞඞ()
+{
+    ඞඞඞ(ඞඞඞඞ);
+}
